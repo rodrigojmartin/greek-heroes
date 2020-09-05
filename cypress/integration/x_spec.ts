a41b928b7b840e5c6cea4cd8e272fdbe591ef85e
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2020-09-05 16:57:28 -0300

    Adds first example acceptance test and fail

---
+++ cypress/integration/x_spec.ts
@@ -0,0 +1,8 @@
+describe('x', () => {
+    it('transforms 2310000.159897 to 2 310 000.16', () => {
+      cy.visit('http://localhost:3000');
+      cy.get('[data-test="inputNumber"]').type("2310000.159897");
+      cy.get('[data-test="submitButton"]').click();
+      cy.get('[data-test="result"]').should('have.value', '2 310 000.16');
+  });
+});
