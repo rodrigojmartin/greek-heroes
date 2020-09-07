9caf06fa2d6457104c8a28260c3ab7666932ebf8
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2020-09-07 12:07:57 -0300

    add handleSave to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-
 import NumberConverterForm from './NumberConverterForm';
 
-class App extends Component  {
+const handleSave = (numberToConvert: string) => {
+  return '2 310 000.16';
+}
+
+class App extends Component {
   render() {
     return(
       <div>
-        <NumberConverterForm />
+        <NumberConverterForm onSave={handleSave}/>
       </div>
     );
   }
