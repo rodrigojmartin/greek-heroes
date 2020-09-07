7e36314d777050c1480a56acf4b7ffcd9a3775a8
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2020-09-07 12:07:35 -0300

    add handleSave to component test

--- src/NumberConverterForm.cy-spec.tsx
+++ src/NumberConverterForm.cy-spec.tsx
@@ -4,16 +4,21 @@ import { mount } from 'cypress-react-unit-test';
 import NumberConverterForm from './NumberConverterForm';
 import * as React from 'react';
 
+
+
 describe('NumberConverterForm', () => {
+
     describe('Clicking the transform button', () => {
         it('Updates the result field', () => {
-            mount(<NumberConverterForm />);
+            const handleOnSave = cy.stub()
+            mount(<NumberConverterForm onSave={handleOnSave}/>);
             
             cy.get('[data-test="inputNumber"]').type("2310000.159897");
             cy.get('[data-test="submitButton"]').click();
-            cy.get('[data-test="result"]').should('have.value', '2 310 000.16');
-
+            expect(handleOnSave).to.have.been.calledWith("2310000.159897");
         })
+
+
 
     })
 
