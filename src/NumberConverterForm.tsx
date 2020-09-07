0f81881a4630c4c93a09db5c01b707bc5b375d40
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2020-09-07 12:08:35 -0300

    Implement component

--- src/NumberConverterForm.tsx
+++ src/NumberConverterForm.tsx
@@ -1,19 +1,54 @@
 import * as React from 'react';
 
-export default class NumberConverterForm extends React.Component {
+interface NumberConverterProps {
+    onSave: (numberToConvert) => string;
+}
+
+interface NumberConverterState {
+    numberToConvert: string;
+    result: string;
+}
+
+export default class NumberConverterForm extends React.Component<NumberConverterProps, NumberConverterState> {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberToConvert: '',
+            result: ''
+        };
+      }
+
+    handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ numberToConvert: event.target.value})
+    }
+
+    handleOnSave = () => {
+        const { numberToConvert } = this.state
+        const { onSave } = this.props
+        var processedResult = onSave(numberToConvert)
+        this.setState({result: processedResult})
+    }
+
     render () {
+        
         return (
           <div>
             <input
                 type="text"
                 data-test="inputNumber"
+                onChange={this.handleTextChange}
+                value={this.state.numberToConvert}
+                
             />
-            <button
+            <button 
                 data-test="submitButton"
+                onClick={this.handleOnSave}
             />
             
             <input
                 data-test="result"
+                value={this.state.result}
             />
             
           </div>
