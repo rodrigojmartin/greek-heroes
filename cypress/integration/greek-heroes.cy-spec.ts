e30fc9a009af1f3de72c539b891c60ba6c98e846
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2020-09-08 14:52:16 -0300

    add scientific notation test

--- cypress/integration/greek-heroes.cy-spec.ts
+++ cypress/integration/greek-heroes.cy-spec.ts
@@ -9,7 +9,7 @@ describe('Greek heroes calculation', () => {
     cy.get('[data-test="submitButton"]').click();
     cy.contains('Result is 2 310 000.16')
   })
-  
+
   it('transforms 1600 to 1600.00', () => {
     cy.get('[data-test="inputNumber"]').type('1600');
     cy.get('[data-test="submitButton"]').click();
@@ -33,6 +33,13 @@ describe('Greek heroes calculation', () => {
     cy.get('[data-test="submitButton"]').click();
     cy.contains('Result is not a number')
   });
+
+  it('transforms a scientific e notation', () => {
+    cy.get('[data-test="inputNumber"]').type('3.456e6');
+    cy.get('[data-test="submitButton"]').click();
+    cy.contains('Result is 3 456 000.00')
+  });
+
 
 });
 
