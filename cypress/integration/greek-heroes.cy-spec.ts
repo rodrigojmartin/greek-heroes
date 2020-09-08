db4ab9daaf940f5e1a0aead7414c3a77bd524c77
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2020-09-08 09:57:19 -0300

    refactor test to use beforeEach

--- cypress/integration/greek-heroes.cy-spec.ts
+++ cypress/integration/greek-heroes.cy-spec.ts
@@ -1,24 +1,26 @@
 describe('Greek heroes calculation', () => {
-    it('transforms 2310000.159897 to 2 310 000.16', () => {
-      cy.visit('http://localhost:3000');
-      cy.get('[data-test="inputNumber"]').type('2310000.159897');
-      cy.get('[data-test="submitButton"]').click();
-      cy.contains('Result is 2 310 000.16')
-    })
     
-    it('transforms 1600 to 1600.00', () => {
-      cy.visit('http://localhost:3000');
-      cy.get('[data-test="inputNumber"]').type('1600');
-      cy.get('[data-test="submitButton"]').click();
-      cy.contains('Result is 1 600.00')
-    });
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  })
+  
+  it('transforms 2310000.159897 to 2 310 000.16', () => {
+    cy.get('[data-test="inputNumber"]').type('2310000.159897');
+    cy.get('[data-test="submitButton"]').click();
+    cy.contains('Result is 2 310 000.16')
+  })
+  
+  it('transforms 1600 to 1600.00', () => {
+    cy.get('[data-test="inputNumber"]').type('1600');
+    cy.get('[data-test="submitButton"]').click();
+    cy.contains('Result is 1 600.00')
+  });
 
-    it('transforms 0 to 0.00', () => {
-      cy.visit('http://localhost:3000');
-      cy.get('[data-test="inputNumber"]').type('0');
-      cy.get('[data-test="submitButton"]').click();
-      cy.contains('Result is 0.00')
-    });
+  it('transforms 0 to 0.00', () => {
+    cy.get('[data-test="inputNumber"]').type('0');
+    cy.get('[data-test="submitButton"]').click();
+    cy.contains('Result is 0.00')
+  });
 
 });
 
