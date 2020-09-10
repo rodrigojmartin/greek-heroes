f8cb359f126abf3d8791e8ebe144bcfc4cf9aaab
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2020-09-10 15:23:29 -0300

    Updates

---
+++ precache-manifest.684a58d2815db8092f4b08266f5ac099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a96be09ac85502a4862025acd1c72d",
+    "url": "/greek-heroes/index.html"
+  },
+  {
+    "revision": "e0054f2f0b199b84a8b4",
+    "url": "/greek-heroes/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8afbaadbd42749b78d3e",
+    "url": "/greek-heroes/static/js/2.9d408955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greek-heroes/static/js/2.9d408955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0054f2f0b199b84a8b4",
+    "url": "/greek-heroes/static/js/main.2eae9e65.chunk.js"
+  },
+  {
+    "revision": "86c75b061df4d2f1ca82",
+    "url": "/greek-heroes/static/js/runtime-main.29863d69.js"
+  }
+]);
