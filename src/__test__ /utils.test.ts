0b14a1ac097fd92730b72de0f65d4a139ce8c2c1
Rodrigo Julian Martin <rmartin@Rodrigos-MacBook-Pro.local>
2020-09-11 16:42:49 -0300

    create test and add list of nasty strings

---
+++ src/__test__ /utils.test.ts
@@ -0,0 +1,17 @@
+/// <reference types="jest" />
+import {convertMoney} from '../utils';
+import Sinkdweller from 'sinkdweller';
+
+//Load the list of nasty strings to be tested
+const nastyStrings: string[] = require("../blns.json");
+
+//For each entry, create a test and check that the function returns a string
+nastyStrings.forEach(element => {
+test(`test the input ${element}`, () => {
+            let fuzzer = new Sinkdweller();
+            let data = Buffer.from(element);
+            let result = fuzzer.fuzzSync(data);
+            let resultString = result.toString('utf8'); 
+            expect(typeof convertMoney(resultString)).toBe('string');          
+        });
+});
--- src/utils.test.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="jest" />
-
-import { convertMoney} from './utils';
-
-test('a', () => {
-    expect(convertMoney('0')).toBe('0.00')
-});
