6e094f4f596981bf7b34fdfa50eb555ca3bc9b19
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2020-09-13 12:32:54 -0300

    add more unit tests

--- src/__tests__/convertMoney.unit.ts
+++ src/__tests__/convertMoney.unit.ts
@@ -1,9 +1,32 @@
 /// <reference types="jest" />
-import {convertMoney} from '../Utils';
+import {convertMoney} from '../utils';
 import nastyStrings from '../../blns.json'
 
 describe('convert money unit tests', ()=> {
 
+
+test('handles 0', () =>{
+    expect(convertMoney('0')).toBe('0.00');
+});
+test('handles integers', () =>{
+    expect(convertMoney('1')).toBe('1.00');
+});
+
+test('handles numbers wih 1 decimal', () =>{
+    expect(convertMoney('1.1')).toBe('1.10');
+});
+
+test('handles numbers wih more than 2 decimals', () =>{
+    expect(convertMoney('568.55555556')).toBe('568.56');
+});
+
+test('handles big numbers', () =>{
+    expect(convertMoney('123456789')).toBe('123 456 789.00');
+});
+
+test('handles negative numbers', () =>{
+    expect(convertMoney('-1000')).toBe('-1 000.00');
+});
 
 test('handles infinity', () =>{
 expect(convertMoney('Infinity')).toBe('∞');
@@ -11,7 +34,7 @@ expect(convertMoney('Infinity')).toBe('∞');
 
 test('handles - infinity', () =>{
     expect(convertMoney('-Infinity')).toBe('-∞');
-    });
+});
 
 //Check the function handles each nasty string in blns.json
 nastyStrings.forEach(element => {
